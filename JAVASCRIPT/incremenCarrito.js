de8925a385c2737775611df6b3a47dd1befafcb9
Marcao22 <160734930+Marcao22@users.noreply.github.com>
2024-02-26 21:22:06 -0500

    carrito
    avanze

---
+++ JAVASCRIPT/incremenCarrito.js
@@ -0,0 +1,18 @@
+
+document.querySelector("#btnRestar").addEventListener("click",restarUno);
+document.querySelector("#btnSumar").addEventListener("click",sumarUno);
+
+let contador=0;
+
+function restarUno(){
+    if (contador > 0)
+    contador= contador-1;
+    document.querySelector("#msgContador").innerHTML = contador
+}
+
+function sumarUno(){
+    contador= contador+1;
+    document.querySelector("#msgContador").innerHTML = contador
+
+}
+
